fb4178f74f218ac06e642769b3b7f066f3488314
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-04 14:06:48 +0200

    staging

--- code/src/GoalPioneers/Formats/TSV/TSVExporter.java
+++ code/src/GoalPioneers/Formats/TSV/TSVExporter.java
@@ -1,6 +1,17 @@
 package GoalPioneers.Formats.TSV;
 
+/**
+ * 
+ */
+
+import GoalPioneers.Formats.Templates.FlatFiles.FlatFilesExporterFacade;
+
+
+/**
+ * 
+ */
 public class TSVExporter 
+        implements FlatFilesExporterFacade
 {
     public TSVExporter()
     {
--- code/src/GoalPioneers/Formats/TSV/TSVImporter.java
+++ code/src/GoalPioneers/Formats/TSV/TSVImporter.java
@@ -1,10 +1,13 @@
 package GoalPioneers.Formats.TSV;
 
+import GoalPioneers.Formats.Templates.FlatFiles.FlatFilesImporterFacade;
+
 
 /**
  * 
  */
 public class TSVImporter 
+        implements FlatFilesImporterFacade
 {
     // Constructor
     public TSVImporter()
